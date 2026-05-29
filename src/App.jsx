f970ffd90e8852f38c05f7e248053748e1784628
Outmane Bzioui <bziouioutmane@gmail.com>
2026-05-29 20:43:50 +0100

    test faceboook redirect

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,39 @@ export default function App() {
   const mediaParam = urlParams.get("media");
 
   const baseTargetSite = import.meta.env.VITE_SITE_URL;
+
   const targetSite = mediaParam
     ? `${baseTargetSite}/${mediaParam}`
     : baseTargetSite;
 
   useEffect(() => {
-    if (targetSite) {
+    const ua = navigator.userAgent || navigator.vendor;
+
+    const isFacebookBrowser =
+      ua.includes("FBAN") || ua.includes("FBAV") || ua.includes("Instagram");
+
+    if (isFacebookBrowser) {
+      // Android Chrome Intent
+      const cleanUrl = targetSite.replace(/^https?:\/\//, "");
+
+      window.location.href = `
+        intent://${cleanUrl}
+        #Intent;
+        scheme=https;
+        package=com.android.chrome;
+        end
+      `.replace(/\s/g, "");
+    } else {
+      // Browser normal
       window.location.href = targetSite;
     }
   }, [targetSite]);
 
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center gap-4">
       <div className="w-8 h-8 border-2 border-gray-300 border-t-gray-900 rounded-full animate-spin"></div>
+
+      <p className="text-sm text-gray-600">Redirecting...</p>
     </div>
   );
 }
